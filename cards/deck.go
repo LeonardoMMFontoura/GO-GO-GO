f4bf5c9b139a89a0c9087f4da55445a5c139496a
Leo <leonardommfontoura@gmail.com>
2021-08-07 15:34:48 -0300

    definicao de deck parcial

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 //Criano um novo tipoe de deck
 // que nada mais é do cque um slice de string
 type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
