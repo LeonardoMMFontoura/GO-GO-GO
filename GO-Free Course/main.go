42579db7e5da99e85a719847ec185c9006fe449a
Leonardo Mastra <leonardommfontoura@gmail.com>
2023-08-30 19:42:49 -0300

    beggining the simple server project

--- GO-Free Course/main.go
+++ GO-Free Course/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	// single-line comments start with "//"
-	// comments are just for documentation - they don't execute
-	fmt.Println("starting Textio server")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
