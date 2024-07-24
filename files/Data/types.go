380b68057bb395b443d91fd0c1b6a9e66f24de85
Leonardo Mastra <leonardommfontoura@gmail.com>
2024-07-24 20:43:05 -0300

    Conversion to miles

--- files/Data/types.go
+++ files/Data/types.go
@@ -11,8 +11,24 @@ var x integer = 12
 
 //if we omit the "=" we create a new type. Why ? For instace: semantics
 type distance float32
+type kilometers float32
+
+// func ToKm(miles distance) kilometers {
+// 	return kilometers(miles * 1.60934)
+// }
+
+//method
+func (miles distance) ToKm() kilometers {
+	return kilometers(miles * 1.60934)
+}
+
+func (km kilometers) ToMiles() distance {
+	return distance(km / 1.60934)
+}
 
 func test() {
 	d := distance(4.5)
+	km := d.ToKm()
+	km.ToMiles()
 	fmt.Println(d)
 }
