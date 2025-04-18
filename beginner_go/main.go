5e155e3196fbf3ccf6462f18989e33e288fb6a55
LeonardoMMFontoura <leonardommfontoura@gmail.com>
2025-04-18 16:49:58 -0300

    slices and for loop with range

--- beginner_go/main.go
+++ beginner_go/main.go
@@ -5,12 +5,25 @@ import (
 )
 
 func main() {
-	numbers := [5]int {10,20,30,40,50} // arrays are imutable once declared, but we can subscribe the elements at index like above
-	numbers[0] = 100 // but we cant grow or shrink the array
-	fmt.Println("This is my array %v", numbers)
+	// allNumbers := numbers[:] //slice copy of our array
+	// firstThree := numbers[0:3] 
+	numbers := []int {10,20,30,40,50}
 
-	matrix := [2][3]int{
-		{1,2,3},
-		{4,5,6},
+	fruits := []string{"apple", "banana","strawberry"}
+	fmt.Printf("This is my fruits: %v\n", fruits)
+
+	fruits = append(fruits, "kiwi")
+	fmt.Printf("This is my new fruits 1 array: %v\n", fruits)
+
+	fruits = append(fruits, "kiwi", "orange", "mango")
+	fmt.Printf("This is my new fruits 2 array: %v\n", fruits)
+
+	moreFruits := []string{"grape", "tomato"}
+	fruits = append(fruits, moreFruits...)
+	fmt.Printf("This is my new fruits 3 array: %v\n", fruits)
+
+
+	for index,value := range numbers{
+		fmt.Printf("Index: %d and value %d\n", index,value)
 	}
 }
