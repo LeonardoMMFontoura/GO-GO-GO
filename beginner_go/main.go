95d35984413b94da2789be7e703aec50744e2d97
LeonardoMMFontoura <leonardommfontoura@gmail.com>
2025-04-18 16:57:41 -0300

    this time with maps

--- beginner_go/main.go
+++ beginner_go/main.go
@@ -3,27 +3,20 @@ package main
 import (
 	"fmt"
 )
-
-func main() {
-	// allNumbers := numbers[:] //slice copy of our array
-	// firstThree := numbers[0:3] 
-	numbers := []int {10,20,30,40,50}
-
-	fruits := []string{"apple", "banana","strawberry"}
-	fmt.Printf("This is my fruits: %v\n", fruits)
-
-	fruits = append(fruits, "kiwi")
-	fmt.Printf("This is my new fruits 1 array: %v\n", fruits)
-
-	fruits = append(fruits, "kiwi", "orange", "mango")
-	fmt.Printf("This is my new fruits 2 array: %v\n", fruits)
-
-	moreFruits := []string{"grape", "tomato"}
-	fruits = append(fruits, moreFruits...)
-	fmt.Printf("This is my new fruits 3 array: %v\n", fruits)
-
-
-	for index,value := range numbers{
-		fmt.Printf("Index: %d and value %d\n", index,value)
+func main(){
+	capitalCities := map[string]string{
+		"USA": "Washington D.C.",
+		"India": "New Delhi",
+		"UK": "London",
 	}
+	fmt.Printf(capitalCities["USA"])
+
+	capital, exists := capitalCities["Germany"]
+	if (exists){
+		fmt.Printf("It is here", capital)
+	}else{
+		fmt.Printf("It is not here")
+	}
+
+	delete(capitalCities, "UK")
 }
